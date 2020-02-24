4cd1f4583af8787ae25233dac343e17f59775a35
Felipe Salles Lopes <felipe28101997@gmail.com>
2020-02-24 14:55:41 -0400

    Criada a classe Players.java

---
+++ src/model/Players.java
@@ -0,0 +1,56 @@
+package model;
+
+public class Players {
+	private String jogador;
+	private int jogos;
+	private int vitorias;
+	private int empate;
+	private int derrotas;
+	private int golsFeitos;
+	private int golsConcedidos;
+	
+	public String getJogador() {
+		return jogador;
+	}
+	public void setJogador(String jogador) {
+		this.jogador = jogador;
+	}
+	public int getJogos() {
+		return jogos;
+	}
+	public void setJogos(int jogos) {
+		this.jogos = jogos;
+	}
+	public int getVitorias() {
+		return vitorias;
+	}
+	public void setVitorias(int vitorias) {
+		this.vitorias = vitorias;
+	}
+	public int getEmpate() {
+		return empate;
+	}
+	public void setEmpate(int empate) {
+		this.empate = empate;
+	}
+	public int getDerrotas() {
+		return derrotas;
+	}
+	public void setDerrotas(int derrotas) {
+		this.derrotas = derrotas;
+	}
+	public int getGolsFeitos() {
+		return golsFeitos;
+	}
+	public void setGolsFeitos(int golsFeitos) {
+		this.golsFeitos = golsFeitos;
+	}
+	public int getGolsConcedidos() {
+		return golsConcedidos;
+	}
+	public void setGolsConcedidos(int golsConcedidos) {
+		this.golsConcedidos = golsConcedidos;
+	}
+	
+	
+}
