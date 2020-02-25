0ccc26bc55e2c1266fc426500884f4cd66cddc41
Felipe Salles Lopes <felipe28101997@gmail.com>
2020-02-24 20:52:58 -0400

    mudanças

--- src/model/dao/PartidasDAO.java
+++ src/model/dao/PartidasDAO.java
@@ -9,7 +9,7 @@ import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import connection.ConnectionFactory;
+import controller.connection.ConnectionFactory;
 import model.bean.Partidas;
 
 public class PartidasDAO {
