dc6fa25a42b6dc136899b26338bfea46cf01b8f9
Felipe Salles Lopes <felipe28101997@gmail.com>
2020-02-24 21:39:36 -0400

    Adição do método toString na classe Partidas

--- src/model/bean/Partidas.java
+++ src/model/bean/Partidas.java
@@ -10,65 +10,83 @@ public class Partidas {
 	private int golsFeitos;
 	private int golsConcedidos;
 	private String timeAdversario;
-	private int penaltisGf;
-	private int penaltisGc;
-	
+	private Integer penaltisGf;
+	private Integer penaltisGc;
+
 	public Date getDataPartida() {
 		return dataPartida;
 	}
+
 	public void setDataPartida(Date dataPartida) {
 		this.dataPartida = dataPartida;
 	}
+
 	public String getModoDeJogo() {
 		return modoDeJogo;
 	}
+
 	public void setModoDeJogo(String modoDeJogo) {
 		this.modoDeJogo = modoDeJogo;
 	}
+
 	public String getAdversario() {
 		return adversario;
 	}
+
 	public void setAdversario(String adversario) {
 		this.adversario = adversario;
 	}
+
 	public String getMeuTime() {
 		return meuTime;
 	}
+
 	public void setMeuTime(String meuTime) {
 		this.meuTime = meuTime;
 	}
+
 	public int getGolsFeito() {
 		return golsFeitos;
 	}
+
 	public void setGolsFeito(int golsFeito) {
 		this.golsFeitos = golsFeito;
 	}
+
 	public int getGolsConcedido() {
 		return golsConcedidos;
 	}
+
 	public void setGolsConcedido(int golsConcedido) {
 		this.golsConcedidos = golsConcedido;
 	}
+
 	public String getTimeAdversario() {
 		return timeAdversario;
 	}
+
 	public void setTimeAdversario(String timeAdversario) {
 		this.timeAdversario = timeAdversario;
 	}
+
 	public int getPenaltisGf() {
 		return penaltisGf;
 	}
+
 	public void setPenaltisGf(int penaltisGf) {
 		this.penaltisGf = penaltisGf;
 	}
+
 	public int getPenaltisGc() {
 		return penaltisGc;
 	}
+
 	public void setPenaltisGc(int penaltisGc) {
 		this.penaltisGc = penaltisGc;
 	}
-	
-	/*public String toString() {
-		return matchId
-	}*/
+
+	public String toString() {
+		return dataPartida + "\t|\t" + modoDeJogo + "\t|\t" + adversario + "\t|\t" + meuTime + "\t\t| " + golsFeitos
+				+ " X " + golsConcedidos + " | " + timeAdversario + "\t|\t" + penaltisGf + "\t|\t" + penaltisGc + "\n";
+	}
 }
